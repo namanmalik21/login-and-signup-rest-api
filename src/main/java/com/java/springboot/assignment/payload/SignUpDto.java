709e46537768e7205fbacded8bfb2a249a58b7b2
Naman Malik <namanmalik21@gmail.com>
2023-05-11 23:57:40 +0530

    Initial commit

---
+++ src/main/java/com/java/springboot/assignment/payload/SignUpDto.java
@@ -0,0 +1,9 @@
+package com.java.springboot.assignment.payload;
+import lombok.Data;
+@Data
+public class SignUpDto {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
